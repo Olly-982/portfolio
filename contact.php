3e06caaec3444396efc56649588702e8798aafc8
Olly-982 <67128001+Olly-982@users.noreply.github.com>
2022-09-07 13:34:02 -0700

    styling changes

--- contact.php
+++ contact.php
@@ -7,20 +7,21 @@
     }
 </style>
 <div class="contact-wrapper">
+    <h1 class="contact-heading text-el">Get in Touch: </h1>
     <form id="contact-form" class="" method="post" action="form-process.php">
-        <?php if ($success) echo "<p class='success' style='font-size: 2em; color: limegreen; margin-top: 3em;'>Thanks for getting in touch! I will get back to you as soon as possible.</p>" ?>
+        <?php if ($success) echo "<p class='success' style='font-size: 2em; color: limegreen; margin-top: 3em; font-weight: 500;'>Thanks for getting in touch! I will get back to you as soon as possible.</p>" ?>
         <div class="contact-details-wrap">
             <div class="input-wrap">
-                <label class="text-el" for="Name">Enter your name: </label>
+                <label class="text-el contact-label" for="Name">Enter your name: </label>
                 <input autofocus class="form-input" type="text" name="Name" id="Name" minlength="2" required>
             </div>
             <div class="input-wrap">
-                <label class="text-el" for="Email">Enter your email: </label>
+                <label class="text-el contact-label" for="Email">Enter your email: </label>
                 <input class="form-input" type="email" name="Email" id="Email" required>
             </div>
         </div>
         <div class="message input-wrap">
-            <label class="text-el" for="Message">Let me know how I can help you: </label>
+            <label class="text-el contact-label" for="Message">Let me know how I can help you: </label>
             <textarea name="Message" id="Message" cols="30" rows="10" maxlength="1000" required></textarea>
         </div>
         <input class="submit" type="submit" value="Submit">
--- scripts/index.js
+++ scripts/index.js
@@ -11,12 +11,15 @@ let allDetails = document.querySelectorAll(".details");
 let contact = document.querySelector(".contact");
 let contactWrap = document.querySelector(".contact-wrapper");
 let aboutWrap = document.querySelector(".about-me-wrap ");
+let header = document.querySelector("header");
+let submit = document.querySelector(".submit");
 
 let logoSvgCls1 = document.querySelectorAll(".cls-1");
 let logoSvgCls2 = document.querySelectorAll(".cls-2");
 let ctas = document.querySelectorAll(".cta");
 
 navToggle.addEventListener("click", () => {
+  header.classList.toggle("reposition");
   if (!navToggle.classList.contains("expanded")) {
     vizzy.style.zIndex = 400;
   } else {
@@ -26,6 +29,13 @@ navToggle.addEventListener("click", () => {
 
 let themeToggle = document.querySelector("#theme-toggle");
 themeToggle.addEventListener("click", () => {
+  let userTheme = localStorage.getItem("PrefersDark");
+  if (!localStorage.getItem("PrefersDark")) {
+    localStorage.setItem("PrefersDark", true);
+  }
+  if (userTheme == "true") {
+    localStorage.removeItem("PrefersDark");
+  }
   themeToggle.classList.toggle("flip");
   burger.classList.toggle("dark-burger");
   allTextEls.forEach((el) => {
@@ -35,14 +45,14 @@ themeToggle.addEventListener("click", () => {
     el.classList.toggle("theme-bg-dark");
   });
   navItems.forEach((el) => {
-    el.classList.toggle("invert-nav-list-items");
+    el.classList.toggle("invert-btn");
   });
   if (
     window.location.href.indexOf("index") > -1 ||
     window.location.href.indexOf("about") > -1
   ) {
     ctas.forEach((el) => {
-      el.classList.toggle("invert-ctas");
+      el.classList.toggle("invert-btn");
     });
     logoSvgCls1.forEach((el) => {
       el.classList.toggle("theme-fill-dark");
@@ -53,7 +63,7 @@ themeToggle.addEventListener("click", () => {
   }
   if (window.location.href.indexOf("portfolio") > -1) {
     portLinks.forEach((el) => {
-      el.classList.toggle("invert-port-links");
+      el.classList.toggle("invert-btn");
     });
     allDetails.forEach((el) => {
       el.classList.toggle("dark-details");
@@ -61,6 +71,9 @@ themeToggle.addEventListener("click", () => {
   }
   if (window.location.href.indexOf("about") > -1) {
     aboutWrap.classList.toggle("dark-details");
+  }
+  if (window.location.href.indexOf("contact") > -1) {
+    submit.classList.toggle("invert-btn");
   }
 });
 
